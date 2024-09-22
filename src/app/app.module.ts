24241e362ec24644bbdb69df7a4107c3395d1111
Natalia Avila Torres <162087085+NataliaAvilaTorres@users.noreply.github.com>
2024-09-22 00:10:18 -0500

    integracion Landin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './LandingPage/header/header.component';
+import { MainComponent } from './LandingPage/main/main.component';
+import { FooterComponent } from './LandingPage/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
