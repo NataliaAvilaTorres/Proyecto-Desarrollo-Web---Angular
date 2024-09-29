0e9156f3c9747d3d5228a05ed8f0afb6980e7df1
Natalia Avila Torres <162087085+NataliaAvilaTorres@users.noreply.github.com>
2024-09-29 00:45:16 -0500

    Falta validacion Login y que el sidebar funcione pero ya se puede navegar

--- src/app/veterinario/veterinario-panel/veterinario-panel.component.ts
+++ src/app/veterinario/veterinario-panel/veterinario-panel.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-veterinario-panel',
   templateUrl: './veterinario-panel.component.html',
   styleUrls: ['./veterinario-panel.component.css']
 })
-export class VeterinarioPanelComponent {
+export class VeterinarioPanelComponent{
 
 }
